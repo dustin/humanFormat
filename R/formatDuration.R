fc7ab10dbd8a007bd4586e3eccc7072ac134202a
Dustin Sallings <dustin@spy.net>
2013-11-23 00:13:24 -0800

    Packaged up formatDuration

---
+++ R/formatDuration.R
@@ -0,0 +1,31 @@
+kNanosecond <- 1
+kMicrosecond <- kNanosecond * 1000
+kMillisecond <- kMicrosecond * 1000
+kSecond <- kMillisecond * 1000
+kMinute <- kSecond * 60
+kHour <- kMinute * 60
+
+formatDurationSmall <- function(ns) {
+    sizes <- c('ns', '\u00b5s', 'ms', 's')
+    e <- ifelse(ns == 0, NA, floor(log(ns, 1000)))
+    suffix <- ifelse(ns == 0, '', sizes[e+1])
+    prefix <- ifelse(ns == 0, '0', sprintf("%g", ns/(1000 ^ floor(e))))
+    paste(prefix, suffix, sep="")
+}
+
+formatDurationHour <- function(ns) {
+	sprintf("%.0fh%s", ns/kHour, formatDurationMinute(ns %% kHour))
+}
+
+formatDurationMinute <- function(ns) {
+	ifelse(abs(ns) > kHour,
+		formatDurationHour(ns),
+		sprintf("%.0fm%ss", ns/kMinute,
+			format((ns %% kMinute) / kSecond, digits=9, scientific=F)))
+}
+
+formatDuration <- function(ns) {
+	ifelse(abs(ns) > kMinute,
+		formatDurationMinute(ns),
+		formatDurationSmall(ns))
+}
---
+++ tests/test-duration.R
@@ -0,0 +1,27 @@
+library(testthat)
+library(humanFormat)
+
+expect_equal("0", formatDuration(0))
+expect_equal("1ns", formatDuration(1 * kNanosecond))
+expect_equal("1.1µs", formatDuration(1100 * kNanosecond))
+expect_equal("2.2ms", formatDuration(2200 * kMicrosecond))
+expect_equal("3.3s", formatDuration(3300 * kMillisecond))
+expect_equal("4m5s", formatDuration(4*kMinute + 5*kSecond))
+expect_equal("4m5.001s", formatDuration(4*kMinute + 5001*kMillisecond))
+expect_equal("5h6m7.001s", formatDuration(5*kHour + 6*kMinute + 7001*kMillisecond))
+expect_equal("8m0.000000001s", formatDuration(8*kMinute + 1*kNanosecond))
+expect_equal("768h4m0.013s",
+	formatDuration(32 * 24 * kHour + 4 * kMinute + 13 * kMillisecond))
+
+expect_equal(c("0", "1ns", "1.1µs", "2.2ms", "3.3s",
+	"4m5.000000000s", "4m5.001000000s", 
+	"5h6m7.001000000s", "8m0.000000001s"),
+formatDuration(c(0,
+	1 * kNanosecond,
+	1100 * kNanosecond,
+	2200 * kMicrosecond,
+	3300 * kMillisecond,
+	4*kMinute + 5*kSecond,
+	4*kMinute + 5001*kMillisecond,
+	5*kHour + 6*kMinute + 7001*kMillisecond,
+	8*kMinute + 1*kNanosecond)))
